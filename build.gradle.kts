2a3fddbb095380e87b7b178a941c40793ed2da60
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-04-28 19:34:43 +0200

    update PaperApi and runServer to 1.20.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.5-R0.1-SNAPSHOT")
     implementation("cloud.commandframework", "cloud-paper", "1.8.4")
     implementation("cloud.commandframework", "cloud-annotations", "1.8.4")
     implementation("cloud.commandframework", "cloud-minecraft-extras", "1.8.4")
@@ -35,7 +35,7 @@ tasks {
     }
 
     runServer {
-        minecraftVersion("1.20.4")
+        minecraftVersion("1.20.5")
     }
 
     shadowJar {
